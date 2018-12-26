3b7d2571549af88985f2efc99ee81ce3f09e2466
Taylor Jones <taylorjonesdev@gmail.com>
2018-12-26 15:17:58 -0700

    Added initial 1.4 java solutions

---
+++ 1_arrays_and_strings/1.4/java/Main.java
@@ -0,0 +1,128 @@
+/**
+ * Question: 1.4
+ * Title: Palindrome Permutation
+ * Description: Given a string, write a function to check if it is a permutation
+ *  of a palindrome. A palindrome is a word or phrase that is the same forwards
+ *  and backwards. A permutation is a rearrangement of letters. The palindrome
+ *  does not need to be limited to just dictionary words.
+ * Example:
+ *    Input: Tact Coa
+ *    Output: True (permutations: "taco cat", "atco cta", etc.)
+ */
+
+ import java.util.HashMap;
+
+ public class Main {
+ 
+    /**
+     * Solution: Normalize the string by converting it to all lowercase and then removing
+     *  any special characters. Then, split the string into a character array and use a
+     *  HashMap to track the # of times each character is present in the array. If the 
+     *  converted string has an even # of characters, it is a palindrome if each character
+     *  is present an even # of times. If the converted string has an odd # of characters,
+     *  it is a palindrome if only one of the characters is present an odd # of times.
+     * 
+     * Runtime: O(n^2)
+     * 
+     * @param s - a String to evaluate
+     * @return boolean - true if the input string is a permutation of a palindrome, false if not.
+     */
+    private static boolean isPalindromePermutation(String s) {
+      int oddCount = 0;
+      HashMap<Character, Integer>map = new HashMap<>();
+
+      // normalize the input string and determine if it's lenght is odd or even.
+      String lowered = s.toLowerCase().replaceAll("[^a-z]","");
+      boolean hasEvenLength = lowered.length() % 2 == 0;
+      char[] chars = lowered.toCharArray();
+
+      // count the # of times each character is present.
+      for (char c : chars) {
+        if (map.containsKey(c)) {
+          map.put(c, map.get(c) + 1);
+        } else {
+          map.put(c, 1);
+        }
+      }
+
+      // determine the # of characters present an odd # of times.
+      for (char c : chars) {
+        if (map.get(c) % 2 != 0) {
+          oddCount += 1;
+        }
+      }
+
+      if ((hasEvenLength && oddCount > 0) || (!hasEvenLength && oddCount != 1)) {
+        return false;
+      }
+
+      return true;
+    }
+
+
+
+    /**
+     * Solution: Similar to the solution above except here, a modification is made to track
+     *  the # of characters that are present an odd # of times in a single pass through the
+     *  character array, which makes the small space sacrifice of using another int to track
+     *  values, but it improves the runtime to O(n) since we only need to loop through the 
+     *  character array once.
+     * 
+     * Runtime: O(n)
+     * 
+     * @param s - a String to evaluate
+     * @return boolean - true if the input string is a permutation of a palindrome, false if not.
+     */
+    private static boolean isPalindromePermutation2(String s) {
+      int oddCount = 0;
+      int currCount = 0;
+      HashMap<Character, Integer>map = new HashMap<>();
+
+      // normalize the input string and determine if it's lenght is odd or even.
+      String lowered = s.toLowerCase().replaceAll("[^a-z]","");
+      boolean hasEvenLength = lowered.length() % 2 == 0;
+      char[] chars = lowered.toCharArray();
+
+      // count the # of times each character is present.
+      for (char c : chars) {
+        if (map.containsKey(c)) {
+          currCount = map.get(c) + 1;
+        } else {
+          currCount = 1;
+        }
+
+        map.put(c, currCount);
+
+        /* to keep a running total of the # of characters that are present an odd # of times,
+         * we'll increment or decrement the # of odd characters found (oddCount) based on the
+         * whether or not the current character has been found an even # of times or not. */ 
+        if (currCount % 2 == 0) {
+          oddCount -= 1;
+        } else {
+          oddCount += 1;
+        }
+      }
+
+      if ((hasEvenLength && oddCount > 0) || (!hasEvenLength && oddCount != 1)) {
+        return false;
+      }
+
+      return true;
+    }
+
+
+
+    public static void main(String[] args) {
+     System.out.println(isPalindromePermutation("Tact Coa")); // true
+     System.out.println(isPalindromePermutation("foo bar"));  // false
+     System.out.println(isPalindromePermutation("taco Cat")); // true
+     System.out.println(isPalindromePermutation("PPTOOST"));  // true
+
+     System.out.println(isPalindromePermutation2("Tact Coa")); // true
+     System.out.println(isPalindromePermutation2("foo bar"));  // false
+     System.out.println(isPalindromePermutation2("taco Cat")); // true
+     System.out.println(isPalindromePermutation2("PPTOOST"));  // true
+   }
+ 
+ }
+ 
--- readme.md
+++ readme.md
@@ -12,12 +12,12 @@ The goal is to solve each problem using each language. However, the over-arching
 
 ### 1. Arrays and Strings
 
-| #  	| Description| C   | C++ | Java  | JS  | Python
+| #  	| Title | C   | C++ | Java  | JS  | Python
 |---  |---  |---  |---  |---  |---  |---  
 |1.1	|Is Unique	|	  |	  |	[Java](1_arrays_and_strings/1.1/java/Main.java)  | [JS](1_arrays_and_strings/1.1/js/main.js)  |  
 |1.2	| Check Permutation	| 	| [C++](1_arrays_and_strings/1.2/cpp/main.cpp)	| [Java](1_arrays_and_strings/1.2/java/Main.java)	|  |  
 |1.3	|	URLify |	| 	|	[Java](1_arrays_and_strings/1.3/java/Main.java)  | [JS](1_arrays_and_strings/1.3/js/main.js) |  
-|1.4  | Palindrome Permutation  | | | | |
+|1.4  | Palindrome Permutation  |   |   | [Java](1_arrays_and_strings/1.4/java/Main.java)  |   |
 |1.5  | One Away  | | | | |
 |1.6	|	String Compression |	| [C++](1_arrays_and_strings/1.6/cpp/main.cpp)	 |	[Java](1_arrays_and_strings/1.6/java/Main.java)  |  |  
 |1.6	| Rotate Matrix	|	  |	  |	  |   |  
@@ -28,7 +28,7 @@ The goal is to solve each problem using each language. However, the over-arching
 
 ### 2. Linked Lists
 
-| #  	| Description| C   | C++ | Java  | JS  | Python
+| #  	| Title | C   | C++ | Java  | JS  | Python
 |---  |---  |---  |---  |---  |---  |---  
 |2.x  | Singly-linked list |  |   | [Java](2_linked_lists/java/LinkedList.java)  |   | 
 |2.x  | Doubly-linked list |  |   |   |   | 
@@ -42,5 +42,17 @@ The goal is to solve each problem using each language. However, the over-arching
 |2.7	|	Intersection |	  |	  |	  |   |  
 |2.8	|	Loop Detection |	  |	  |	  |   |  
 
-
 \* Some of the solutions are in the same file as the their corresponding linked-list implementations.
+
+<br>
+
+### 3. Stacks and Queues
+
+| #  	| Title | C   | C++ | Java  | JS  | Python
+|---  |---  |---  |---  |---  |---  |---  
+|3.1	|	Three in One |	  |	  |	  |   |  
+|3.2	|	Stack Min |	  |	  |	  |   |  
+|3.3	|	Stack of Plates |	  |	  |	  |   |  
+|3.4	|	Queue via Stacks |	  |	  |	  |   |  
+|3.5	|	Sort Stack |	  |	  |	  |   |  
+|3.6	|	Animal Shelter |	  |	  |	  |   |  
