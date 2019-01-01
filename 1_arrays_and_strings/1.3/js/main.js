33cea83d0018c2c8f6a6f06c9271cb6a71f0fd7c
Taylor Jones <taylorjonesdev@gmail.com>
2019-01-01 15:26:42 -0600

    Added python implementation of 1.3

--- 1_arrays_and_strings/1.3/js/main.js
+++ 1_arrays_and_strings/1.3/js/main.js
@@ -1,5 +1,6 @@
 /**
  * Question: 1.3
+ * Name: URLify
  * Description: Write a method to replace all spaces in a string with '%20'. You may assume that
  *  the string has sufficient space at the end of the string to hold the additional characters, and
  *  that you are given the 'true' length of the string.
