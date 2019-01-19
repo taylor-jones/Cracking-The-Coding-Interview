2d28a1b718e429c13db4231213a1ccf21508d75a
Taylor Jones <taylorjonesdev@gmail.com>
2019-01-18 18:56:30 -0600

    Updated readme to point to 2.5 java solution

--- 2_linked_lists/java/Main.java
+++ 2_linked_lists/java/Main.java
@@ -8,66 +8,68 @@ import java.util.HashSet;
  */
 public class Main {
   public static void main(String[] args) {
-    // SinglyLinkedList list = new SinglyLinkedList();
-    // SinglyLinkedListNode fiveNode = new SinglyLinkedListNode(5);  // used for 2.3
-    // SinglyLinkedListNode nineNode = new SinglyLinkedListNode(9);  // used for 2.3
+    SinglyLinkedList list = new SinglyLinkedList();
+    SinglyLinkedListNode fiveNode = new SinglyLinkedListNode(5);  // used for 2.3
+    SinglyLinkedListNode nineNode = new SinglyLinkedListNode(9);  // used for 2.3
 
-    // list.addAtHead(fiveNode);
-    // list.addAtHead(4);
-    // list.addAtHead(4);
-    // list.addAtHead(2);
-    // list.addAtHead(6);
-    // list.addAtHead(6);
-    // list.addAtTail(8);
-    // list.addAtTail(9);
-    // list.print();
+    list.addAtHead(fiveNode);
+    list.addAtHead(4);
+    list.addAtHead(4);
+    list.addAtHead(2);
+    list.addAtHead(6);
+    list.addAtHead(6);
+    list.addAtTail(8);
+    list.addAtTail(9);
+    list.print();
 
-    // list.removeAtHead();
-    // list.removeAtTail();
-    // list.removeAtTail();
-    // list.removeAtTail();
-    // list.removeAtTail();
-    // list.print();
+    list.removeAtHead();
+    list.removeAtTail();
+    list.removeAtTail();
+    list.removeAtTail();
+    list.removeAtTail();
+    list.print();
 
-    // list.removeAtTail();
-    // list.print();
+    list.removeAtTail();
+    list.print();
 
-    // list.has(2);
-    // list.has(3);
+    list.has(2);
+    list.has(3);
 
-    // list.removeDups();
-    // list.print();
+    list.removeDups();
+    list.print();
 
-    // list.deleteNode(fiveNode);
-    // list.deleteNode(nineNode);
-    // list.print();
+    list.deleteNode(fiveNode);
+    list.deleteNode(nineNode);
+    list.print();
 
-    // list.addAtHead(4);
-    // list.addAtHead(3);
-    // list.addAtHead(2);
-    // list.addAtHead(1);
-    // list.addAtTail(5);
-    // list.addAtTail(1);
-    // list.addAtTail(2);
-    // list.addAtTail(3);
-    // list.addAtTail(4);
-    // list.print();
+    list.addAtHead(4);
+    list.addAtHead(3);
+    list.addAtHead(2);
+    list.addAtHead(1);
+    list.addAtTail(5);
+    list.addAtTail(1);
+    list.addAtTail(2);
+    list.addAtTail(3);
+    list.addAtTail(4);
+    list.print();
 
-    // list.partition(3);
-    // list.print();
+    list.partition(3);
+    list.print();
 
-    // System.out.println(list.nthFromLast(0));
-    // System.out.println(list.nthFromLast(1));
-    // System.out.println(list.nthFromLast(2));
-    // System.out.println(list.nthFromLast(3));
-    // System.out.println(list.nthFromLast(4));
+    System.out.println(list.nthFromLast(0));
+    System.out.println(list.nthFromLast(1));
+    System.out.println(list.nthFromLast(2));
+    System.out.println(list.nthFromLast(3));
+    System.out.println(list.nthFromLast(4));
 
-    // System.out.println(list.nthFromLast2(0));
-    // System.out.println(list.nthFromLast2(1));
-    // System.out.println(list.nthFromLast2(2));
-    // System.out.println(list.nthFromLast2(3));
-    // System.out.println(list.nthFromLast2(4));
+    System.out.println(list.nthFromLast2(0));
+    System.out.println(list.nthFromLast2(1));
+    System.out.println(list.nthFromLast2(2));
+    System.out.println(list.nthFromLast2(3));
+    System.out.println(list.nthFromLast2(4));
 
+
+    System.out.println("\nTesting - 2.5 - Sum Lists");
 
     SinglyLinkedList list1 = new SinglyLinkedList();
     SinglyLinkedList list2 = new SinglyLinkedList();
